85d32e647c81f6faea8995188415083d75185124
Shayne Monahan <shayne.monahan@gmail.com>
2024-02-13 13:16:32 +0000

    Add React Notes App tutorial

---
+++ src/useLocalStorage.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export function useLocalStorage<T>(key: string, initalValue: T | (() => T)) {
+  const [value, setValue] = useState<T>(() => {
+    const jsonValue = localStorage.getItem(key);
+    if (jsonValue === null) {
+      if (typeof initalValue === "function") {
+        return (initalValue as () => T)();
+      } else {
+        return initalValue;
+      }
+    } else {
+      return JSON.parse(jsonValue);
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  });
+  return [value, setValue] as [T, typeof setValue];
+}
